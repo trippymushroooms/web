a484ed777d282656db3f7eecdf58305954e6b2ad
JasonJForitk <2020152672@ub.edu.bz>
2020-09-20 13:59:06 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function textOn() {
+    getElementById('notOn').innerHTML = 'Not On';
+    getElementById('notOn').src('/assets/notOn.png');
+}
+
+function textOff() {
+    getElementById('notOn').innerHTML = 'Not Off';
+    getElementById('notOn').src('/assets/notOff.png');
+}
