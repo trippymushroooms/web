1cc8b05205dc6beb6e7c86da6ab25734af4f7528
JasonJForitk <2020152672@ub.edu.bz>
2020-09-21 09:00:14 -0600

    version 2

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function textOn() {
-    getElementById('notOn').innerHTML = 'Not On';
-    getElementById('notOn').src('/assets/notOn.png');
-}
-
-function textOff() {
-    getElementById('notOn').innerHTML = 'Not Off';
-    getElementById('notOn').src('/assets/notOff.png');
-}
